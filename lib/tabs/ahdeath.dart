367d400f96b08ce6345f668dc797be3234aab78c
ahmedShabaan11 <145864594+ahmedShabaan11@users.noreply.github.com>
2025-04-08 00:01:22 +0200

    n

--- lib/tabs/ahdeath.dart
+++ lib/tabs/ahdeath.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:islamic_app/my_theme.dart';
 
 class AhdeathScreen extends StatelessWidget {
-  const AhdeathScreen({super.key});
+  AhdeathScreen({super.key});
+
+  List<String> ahadeeth = [];
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.yellow,);
+    return Column(
+      children: [
+        Image.asset("assets/images/basmala.png"),
+        Divider(
+          color: Theme.of(context).primaryColor,
+          thickness: 3,
+        ),
+        Text(
+          "Ahadeeth ",
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        Divider(
+          color: Theme.of(context).primaryColor,
+          thickness: 3,
+        ),
+        ListView.separated(
+            itemBuilder: (context, index) {
+              return Text("data");
+            },
+            separatorBuilder: (context, index) {
+              return Container(
+                height: 1,
+                margin: EdgeInsets.symmetric(horizontal: 50),
+                color: MyThemeData.primaryColor,
+              );
+            },
+            itemCount: ahadeeth.length)
+      ],
+    );
   }
 }
